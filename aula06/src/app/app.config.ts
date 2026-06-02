2a8bde7205730a0d1cbc087670deed100dec0b40
PauloFerreiraGit <pauloferreiraworkflow@gmail.com>
2026-06-02 21:44:00 +0100

    environment do projeto

--- aula06/src/app/app.config.ts
+++ aula06/src/app/app.config.ts
@@ -3,10 +3,15 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { initializeApp } from 'firebase/app';
+import { environment } from '../environment/environment';
+
+export const  app = initializeApp(environment.firebaseConfig);
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
-  ]
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+  ],
 };
---
+++ aula06/src/environment/environment.prod.ts
@@ -0,0 +1,13 @@
+import { Keys } from './environment.Keys';
+
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: Keys.apiKey,
+    authDomain: 'frontendcinel-paulo.firebaseapp.com',
+    projectId: 'frontendcinel-paulo',
+    storageBucket: 'frontendcinel-paulo.firebasestorage.app',
+    messagingSenderId: '126445201275',
+    appId: '1:126445201275:web:17311e4de9aa8379123fde',
+  },
+};
