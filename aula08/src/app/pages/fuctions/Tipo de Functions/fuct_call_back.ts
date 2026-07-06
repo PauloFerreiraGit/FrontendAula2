4a02ac64ec5c1ec3300db6e57b17f0cbde3a72f2
PauloFerreiraGit <pauloferreiraworkflow@gmail.com>
2026-07-06 22:44:46 +0100

    fim da aula08

--- aula08/src/app/pages/fuctions/Tipo de Functions/fuct_call_back.ts
+++ aula08/src/app/pages/fuctions/Tipo de Functions/fuct_call_back.ts
@@ -1,8 +1,17 @@
 /// funções callbacks são funções que retornam outra função.
 
 export function processar(valor: number, callback: (n: number) => void) {
+    console.log('Processando.....');
     callback(valor);
 
 }
 
-processar(10, (n) => console.log('valor processado: $(n)'));
+processar(10, (n) => console.log('valor processado: ${n + 10}'));
+
+
+export function saudacao() {
+   console.log("Bem Vindo!");
+
+}
+
+setTimeout(saudacao, 3000); // Executa após 3 segundos
